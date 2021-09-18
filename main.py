bd446c5edfdfe906237cd76fb3c3c781dd8d59c3
TheRealRondon <amirhossein2324236@gmail.com>
2021-09-18 14:41:33 +0430

    Some modification for deployment.

--- main.py
+++ main.py
@@ -71,9 +71,4 @@ def requestFxAPI():
     else:
       return success_response(res)
 
-# Running the fask server
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
-
-
 
