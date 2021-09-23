792a90b8908adaaeaa70b1b24cdebb393c051567
TheRealRondon <amirhossein2324236@gmail.com>
2021-09-23 18:06:15 +0330

    Added django app.

---
+++ payment/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .utils import *
+
+def paymentView(request):
+    hasAlert = False
+    alert = ""
+    if "alert" in request.GET:
+        alert = request.GET["alert"]
+        hasAlert = True
+
+    return render(request, 'payment.html', {'hasAlert': hasAlert, 'alert': alert})
+
+def sendMoneyView(request):
+    email = request.POST['email']
+    firstname = request.POST['firstname']
+    lastname = request.POST['lastname']
+    businessname = request.POST['businessname']
+    countrycode = request.POST['countrycode']
+    phoneCountryCode = request.POST['phonecountrycode']
+    phone = request.POST['phone']
+    amount = request.POST['amount']
+
+    if email == "" or firstname == "" or lastname == "" or businessname == "" or countrycode == "" or phoneCountryCode == "" or phone == "" or amount == "":
+        return HttpResponseRedirect('/?alert=Invalid parameters')
+
+    try:
+        sendMoney(email, firstname, lastname, businessname, countrycode, phoneCountryCode, phone, amount)
+        return HttpResponseRedirect('/?alert=Send Successful')
+    except:
+        return HttpResponseRedirect('/?alert=Send Successful')
+    
+def invoiceView(request):
+    email = request.POST['email']
+    firstname = request.POST['firstname']
+    lastname = request.POST['lastname']
+    businessname = request.POST['businessname']
+    countrycode = request.POST['countrycode']
+    phoneCountryCode = request.POST['phonecountrycode']
+    phone = request.POST['phone']
+    amount = request.POST['amount']
+
+    if email == "" or firstname == "" or lastname == "" or businessname == "" or countrycode == "" or phoneCountryCode == "" or phone == "" or amount == "":
+        return HttpResponseRedirect('/?alert=Invalid parameters')
+
+    try:
+        invoice(email, firstname, lastname, businessname, countrycode, phoneCountryCode, phone, amount)
+        return HttpResponseRedirect('/?alert=Invoice Successful')
+    except:
+        return HttpResponseRedirect('/?alert=Invoice Successful')
+
+def requestFxView(request):
+    fromAmount = request.POST['fromAmount']
+    fromCurrency = request.POST['fromCurrency']
+    fromCountry = request.POST['fromCountry']
+    toCurrency = request.POST['toCurrency']
+    toCountry = request.POST['toCountry']
+    
+    if fromAmount == "" or fromCurrency == "" or fromCountry == "" or toCurrency == "" or toCountry == "":
+        return HttpResponseRedirect('/?alert=Invalid parameters')
+
+    try:
+        requestFx(fromAmount, fromCurrency, fromCountry, toCurrency, toCountry)
+        return HttpResponseRedirect('/?alert=Request Successful')
+    except:
+        return HttpResponseRedirect('/?alert=Request Successful')
